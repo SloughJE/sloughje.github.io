43b37946751891b119949de7e886982b7381f59a
SloughJE <SloughJE@users.noreply.github.com>
2024-05-18 22:15:50 +0000

    deploy: c0d1cef0099b8b62dc14427a1279e9cb69e85045

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715895281',resources: [
+    cacheName: 'chirpy-1716070547',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
